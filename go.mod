5c2527bc507e2c748ed5ff1c9e2c9948d6ac172f
alexandresgf <alexandresgf@gmail.com>
2020-07-31 22:24:17 -0300

    Add multilog output and code improvements

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/ahmdrz/goinsta/v2 v2.4.5
+	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-colorable v0.1.7
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
--- main.go
+++ main.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"io"
+	"os"
+	"strconv"
+
 	"github.com/grindlabs/sorteiagram/cmd"
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func init() {
-	log.SetLevel(log.DebugLevel)
+	if err := godotenv.Load(); err != nil {
+		panic("Unable to load the env file")
+	}
+
+	debug, err := strconv.Atoi(os.Getenv("DEBUG"))
+
+	if err != nil {
+		panic("Unable to convert the DEBUG env var")
+	}
+
+	log.SetLevel(log.InfoLevel)
+
+	if debug == 1 {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+		DisableColors: true,
+	})
+
+	multiWriter := io.MultiWriter(os.Stdout, &lumberjack.Logger{
+		Filename:   "logs/sorteiagram.log",
+		MaxSize:    50,
+		MaxBackups: 3,
+		MaxAge:     28,
+		Compress:   true,
+	})
+
+	log.SetOutput(multiWriter)
 }
 
 func main() {
